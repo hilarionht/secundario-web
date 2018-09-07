a53248d54d32585c427a3d4e48a79c84e745edad
HILARION <trejo.hilarion@gmail.com>
2018-09-07 16:49:21 -0300

    add list cals

--- src/app/models/calificacionResult.ts
+++ src/app/models/calificacionResult.ts
@@ -3,7 +3,10 @@ export class CalificacionResult{
         public puntos: string,
         public categoria: string,
         public nombre: string,
+        public apellido:string,
         public foto: string,
+        public numero: string,
+        public criterio: string,
         public id?: string
     ) {
 
--- src/app/routes/eleccion/calificacion/calificaciones/calificaciones.component.ts
+++ src/app/routes/eleccion/calificacion/calificaciones/calificaciones.component.ts
@@ -7,13 +7,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './calificaciones.component.html',
 })
 export class CalificacionesComponent implements OnInit {
-  calificaiones:CalificacionResult[]=[];
+  reinas:CalificacionResult[]=[];
+  simpatias:CalificacionResult[]=[];
+  elegancia:CalificacionResult[]=[];
   constructor(
     public _calificacionService: CalificacionService
   ) 
   { 
     this._calificacionService.result().subscribe((res:any) => {
-      this.calificaiones = res
+       this.reinas = res.reinaAndPrincesas;
+        this.simpatias = res.missSimpatia;
+        this.elegancia = res.reinaAndPrincesas;
+      console.log(res);
+      
     });
   }
 
